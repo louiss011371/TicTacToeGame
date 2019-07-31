3e1a46d003a81d3326decdab3414ded18793689c
louiss011371 <louiss011371@hotmail.com>
2019-07-31 15:16:37 +0800

    Initial Commit

---
+++ Click/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Click
+//
+//  Created by Louis Tsui on 31/7/2019.
+//  Copyright © 2019 Louis Tsui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
